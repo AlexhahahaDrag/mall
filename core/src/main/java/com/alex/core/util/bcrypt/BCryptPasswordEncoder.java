58bdd173151dcec1b9e086fdf413cedcc4a145a6
AlexhahahaDrag <734663446@qq.com>
2019-08-02 17:15:40 +0800

    b crypt

--- core/src/main/java/com/alex/core/util/bcrypt/BCryptPasswordEncoder.java
+++ core/src/main/java/com/alex/core/util/bcrypt/BCryptPasswordEncoder.java
@@ -2,6 +2,7 @@ package com.alex.core.util.bcrypt;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.bouncycastle.crypto.generators.BCrypt;
 
 import java.security.SecureRandom;
 import java.util.regex.Pattern;
@@ -21,5 +22,13 @@ public class BCryptPasswordEncoder {
 
     private final SecureRandom random;
 
-    private Pattern BCRYPT_PATTERN = Pattern.compile("\\A\\$2a?\\$\\d\\d\\$[./0-9A-Za-z]{53}]");
+    private Pattern BCRYPT_PATTERN = Pattern.compile("\\A\\$2a?\\$\\d\\d\\$[./0-9A-Za-z]{53}");
+
+    public BCryptPasswordEncoder(int strength) {
+        this(strength, null);
+    }
+
+    public BCryptPasswordEncoder(int strength, SecureRandom random) {
+        if (strength != -1 && strength < BCrypt.M)
+    }
 }
